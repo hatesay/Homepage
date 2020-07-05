a6a88e6f02f0a083a6968395e25d801926cc7fbd
hatesay <2390292435@qq.com>
2020-07-05 17:17:53 +0800

    ...

--- js/Next.js
+++
@@ -1,5 +0,0 @@
-funtion Next (minNum,maxNum) {
-	number = Math.floor(Math.random()*(max-min+1)+min);
-	var bg = document.getElementById("bg");
-	bg.style.backgroundImage="url("Blackground/background"+number+".png")";
-}
